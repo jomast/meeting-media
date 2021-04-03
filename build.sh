c6f98e26aa7215d6c0da77187d2fbc1fac98f35e
Jonathan Stanford <jomast@stanfordscomputer.com>
2021-04-02 22:12:52 -0400

    update build flags

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ CGO_ENABLED=1 GOARCH=arm64 go build -ldflags="-s -w" -o meeting-media-arm64
 # merge both bins together to make universal binary
 lipo -create -output meeting-media meeting-media-arm64 meeting-media-amd64
 
-# create macos app
+# create MacOS app
 appify -author "Jonathan Stanford" -icon icon.png -id "MeetingMedia" -name "MeetingMedia" -version "1.1" ./meeting-media
