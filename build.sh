116391f89741cc664959de967e7ef886c98f2ff8
Jonathan Stanford <jomast@stanfordscomputer.com>
2021-04-07 09:27:32 -0400

    Update build logic
    Divid macos and linux build logic

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #/bin/sh
 
-# build for amd64 and arm64
-CGO_ENABLED=1 GOARCH=amd64 go build -ldflags="-s -w" -o meeting-media-amd64
-CGO_ENABLED=1 GOARCH=arm64 go build -ldflags="-s -w" -o meeting-media-arm64
+if [ "$(uname -s)" = "Darwin" ]; then
 
-# merge both bins together to make universal binary
-lipo -create -output meeting-media meeting-media-arm64 meeting-media-amd64
+  # build for amd64 and arm64
+  CGO_ENABLED=1 GOARCH=amd64 go build -ldflags="-s -w" -o meeting-media-amd64
+  CGO_ENABLED=1 GOARCH=arm64 go build -ldflags="-s -w" -o meeting-media-arm64
 
-# create MacOS app
-appify -author "Jonathan Stanford" -icon icon.png -id "MeetingMedia" -name "MeetingMedia" -version "1.1" ./meeting-media
+  # merge both bins together to make universal binary
+  lipo -create -output meeting-media meeting-media-arm64 meeting-media-amd64
+
+  # create MacOS app
+  go get -u github.com/machinebox/appify
+  appify -author "Jonathan Stanford" -icon icon.png -id "MeetingMedia" -name "MeetingMedia" -version "1.2" ./meeting-media
+
+  rm meeting-media*
+
+else
+
+  CGO_ENABLED=1 go build -ldflags="-s -w" -o meeting-media
+
+fi
